3d3a619aa0ecc2ed5649f3c719065352585ee798
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-05-27 17:18:10 -0700

    pseudo code for javascript completed

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// pseudo coding:
+    // user is shown random number at start of game (between 19 - 120)
+    // when user clicks on button:
+        // add that buttons specific amount of points to the user's total score
+        // each button has a random hidden value between 1 - 12
+        // when button is clicked, update the score counter
+    // if user score = winning score (randumly generated num), then
+        // wins ++
+        // restart game
+    // if user score > winning score (randomly generated num), then
+        // losses ++
+        // restart game
+
+
+// declare the global variables
+var wins = 0;
+var losses = 0;
+var userScore = 0;
+var winScore = 0;
+var clickCounter = 0;
+
+// function for starting new game
+function resetGame() {
+
+}
+
+// function for updating elements in HTML to display
+function updateDisplay() {
+    
+}
+
+// function for checking if they win or lose
+
+// start the game on refresh/restart 
+
+// function for making sure document is ready
+
+// add the event click listener
+
